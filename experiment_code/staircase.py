1a54b2204a3520d7fb610136c18d2c01a2b05611
Maja Friedemann <maja.friedemann@gmail.com>
2024-02-12 15:33:51 +0000

    dat

--- experiment_code/staircase.py
+++ experiment_code/staircase.py
@@ -8,6 +8,7 @@ from psychopy.hardware import keyboard
 from mpydev import BioPac  # MAJA
 import random
 import math
+import json
 
 ###################################
 # SESSION INFORMATION
@@ -411,7 +412,7 @@ def do_trial(win, mouse, info, gv, DUMMY, mp, effort_outline, effort_fill, effor
             win.flip(), exit_q(), core.wait(2.4)
 
     info['total_reward'] = int(info['total_reward']) + int(info['reward_earned'])
-    info['effort_trace'] = str(effort_trace)
+    info['effort_trace'] = json.dumps(effort_trace)
     info['participant_effort_response_time'] = elapsed_time
     # get updated info dict back out
     return info
@@ -570,7 +571,7 @@ def do_calibration(win, mouse, info, gv, DUMMY, mp, calibration_txt, welcome_txt
     info['max_effort_calibration_3'] = max_efforts[2]
     info['max_effort'] = max_effort
     info['max_effort_baseline_corrected'] = max_effort - gripper_zero_baseline
-    info['effort_trace'] = str(effort_trace)
+    info['effort_trace'] = json.dumps(effort_trace)
 
     welcome_txt.text = f"Well done!"
     draw_all_stimuli([welcome_txt])
